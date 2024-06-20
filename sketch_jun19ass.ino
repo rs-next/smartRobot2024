33ee6837c233617514e65ca8489fa0ab447c9448
seunghyun <rs_next@naver.com>
2024-06-21 02:49:57 +0900

    이동 로직 수정

--- sketch_jun19ass.ino
+++ sketch_jun19ass.ino
@@ -1,10 +1,10 @@
 #include <Wire.h>
 #include <HUSKYLENS.h>
 #include <PRIZM.h> // 테트릭스 프리즘 보드 초기화
-#define IRR 4 // 정면부 라인트레이서 오른쪽
-#define IRL 5 // 정면부 라인트레이서 왼쪽
-#define BIRL 3 // 측면부 라인트레이서 왼쪽
-#define BIRR 2 // 측면부 라인트레이서 오른쪽
+#define FR A2 // 정면부 라인트레이서 오른쪽
+#define FL A3 // 정면부 라인트레이서 왼쪽
+#define BR 3 // 측면부 라인트레이서 왼쪽
+// #define BL 2 // 측면부 라인트레이서 오른쪽
 #define ARRAY_SIZE 3
 #define HUSKYLENS_ADDRESS 0x32
 
@@ -15,9 +15,12 @@ PRIZM prizm; // 메인 프리즘 보드
 EXPANSION exc; // 익스펜션 컨트롤러 
 HUSKYLENS huskylens;
 
+int firstMoving = 0;
 int objectColorCount = 0;
 int robot_x = 0;
 int robot_y = 0;
+int middleValue = 512;
+int yFlag = 0;
 
 struct Pillar {
     Color color;
@@ -42,36 +45,52 @@ Color checkColor(int id) {
     }
 }
 
+bool isColorInArray(Color color) {
+    for (int i = 0; i < objectColorCount; i++) {
+        if (objectArray[i] == color) {
+            return true;
+        }
+    }
+    return false;
+}
+
 void forward() {
-    exc.setMotorSpeeds(1, -150, 150);
-    exc.setMotorSpeeds(2, 150, -150);
+    exc.setMotorSpeeds(1, -100, 100);
+    exc.setMotorSpeeds(2, 100, -100);
 }
 
 void backward() {
-    exc.setMotorSpeeds(1, 150, -150);
-    exc.setMotorSpeeds(2, -150, 150);
+    exc.setMotorSpeeds(1, 100, -100);
+    exc.setMotorSpeeds(2, -100, 100);
 }
 
 void turnRight() {
-    exc.setMotorSpeeds(1, -150, 150);
-    exc.setMotorSpeeds(2, -150, 150);
+    exc.setMotorSpeeds(1, -100, 100);
+    exc.setMotorSpeeds(2, -100, 100);
 }
 
 void turnLeft() {
-    exc.setMotorSpeeds(1, 150, -150);
-    exc.setMotorSpeeds(2, 150, -150);
+    exc.setMotorSpeeds(1, 100, -100);
+    exc.setMotorSpeeds(2, 100, -100);
 }
 
 void goingRight() {
-    exc.setMotorSpeeds(1, -150, -150);
-    exc.setMotorSpeeds(2, -150, -150);
+    exc.setMotorSpeeds(1, -100, -100);
+    exc.setMotorSpeeds(2, -100, -100);
 }
 
 void goingLeft() {
-    exc.setMotorSpeeds(1, 150, 150);
-    exc.setMotorSpeeds(2, 150, 150);
+    exc.setMotorSpeeds(1, 90, 90);
+    exc.setMotorSpeeds(2, 90, 90);
 }
-
+void goingBack() {
+    exc.setMotorSpeeds(1, 65, -65);
+    exc.setMotorSpeeds(2, -65, 65);
+}
+void goingFront() {
+    exc.setMotorSpeeds(1, -65, 65);
+    exc.setMotorSpeeds(2, 65, -65);
+}
 void motorStop() {
     exc.setMotorSpeeds(1, 0, 0);
     exc.setMotorSpeeds(2, 0, 0);  
@@ -84,72 +103,10 @@ bool isArrayEmpty() {
 bool isArrayFull() {
     return objectColorCount == ARRAY_SIZE;
 }
-
-void setup() {
-    Serial.begin(9600);
-    Wire.begin();
-
-    while (!huskylens.begin(Wire)) {
-        Serial.println("HuskyLens 초기화 실패. 연결을 확인하세요.");
-        Serial.println("1. HuskyLens의 'Protocol Type' 설정이 I2C인지 확인하세요.");
-        Serial.println("2. HuskyLens와 Tetrix Prizm 보드 간의 연결을 확인하세요.");
-        delay(1000); // 1초 대기
-    }
-    Serial.println("HuskyLens 초기화 성공!");
-
-    // 기본 보드 및 보조 컨트롤러 초기화
-    prizm.PrizmBegin();
-    prizm.setMotorInvert(2, 1);
-    exc.setMotorInvert(1, 1, 1);
-    prizm.setServoSpeed(3, 50);
-    prizm.setServoPosition(3, 60);
-    exc.setMotorInvert(2, 1, 1);
-
-    // 정면부 라인트레이서 초기화
-    pinMode(IRL, INPUT);
-    pinMode(IRR, INPUT);
-    pinMode(BIRL, INPUT);
-    pinMode(BIRR, INPUT);
-}
-
-void checkLineTraisor() { // 라인트레이서 인식 메소드
-    Serial.println("Starting line tracing...");
-
-    while (true) {
-        Serial.println("Checking sensors...");
-        // 정면부 센서 값 시리얼 모니터에 출력
-        Serial.print("F_Left Sensor: ");
-        Serial.print(digitalRead(IRL));
-        Serial.print(" | Right Sensor: ");
-        Serial.println(digitalRead(IRR));
-
-        // 측면부 센서 값 시리얼 모니터 출력
-        Serial.print("B_Left Sensor: ");
-        Serial.print(digitalRead(BIRL));
-        Serial.print(" | B_Right Sensor: ");
-        Serial.println(digitalRead(BIRR));
-        
-        delay(100);
-
-        // 양쪽 라인 센서 발견시
-        if ((digitalRead(IRL) == HIGH && digitalRead(IRR) == HIGH 
-            && digitalRead(BIRL) == HIGH && digitalRead(BIRR) == HIGH) 
-            || (digitalRead(BIRL) == HIGH && digitalRead(BIRR) == HIGH)) {
-            motorStop();
-            Serial.println("Line detected, stopping motors.");
-            if (robot_x == 0) {      
-                robot_y += 1;
-            } else if (robot_x == 1) {
-                robot_y -= 1;
-            }
-            Serial.print("현재 로봇좌표 : X : ");
-            Serial.println(robot_x);
-            Serial.print("현재 로봇좌표 : Y : ");
-            Serial.println(robot_y);
-
-            // 허스키 렌즈를 통한 색상 체크 
-            if (huskylens.request()) {     
+void checkHuskyLens(){
+       if (huskylens.request()) {     
                 Serial.println("HuskyLens request successful.");
+                delay(2000);
                 if (huskylens.count() > 1) {    
                     Serial.println("Objects detected.");
                     // 오브젝트 인식
@@ -158,7 +115,7 @@ void checkLineTraisor() { // 라인트레이서 인식 메소드
 
                     // 기둥 및 오브젝트 구분
                     HUSKYLENSResult pillarObject, Object;
-                    if (result1.yCenter < result2.yCenter) {
+                    if (result1.yCenter > result2.yCenter) {
                         pillarObject = result1;
                         Object = result2;
                     } else {
@@ -194,64 +151,113 @@ void checkLineTraisor() { // 라인트레이서 인식 메소드
                             break;
                     }
 
-                    if (objectColorCount < ARRAY_SIZE) {
-                        objectArray[objectColorCount++] = ObjectColor;
-                    } else {
-                        Serial.println("Color array limit exceeded.");
-                        Serial.println("컬러 배열 한도초과");
-                    }
-                    delay(3000);
-                    if (digitalRead(IRL) == LOW && digitalRead(IRR) == LOW) {
-                        break;
-                    }
-                    goingLeft();
-                    delay(500);
-                }
+                   if (!isColorInArray(ObjectColor)) {
+                      if (objectColorCount < ARRAY_SIZE) {
+                          objectArray[objectColorCount++] = ObjectColor;
+                      } else {
+                          Serial.println("Color array limit exceeded.");
+                          Serial.println("컬러 배열 한도초과");
+                      }
+                  } else {
+                      Serial.println("ObjectColor already in array.");
+                      Serial.println("ObjectColor가 배열에 이미 존재합니다.");
+                  }
+               }
             }
-        }
-        goingLeft();
-        delay(100);
-    }
-
-    // 끝라인 도착시 회전
-    if (digitalRead(IRL) == LOW && digitalRead(IRR) == LOW &&
-        digitalRead(BIRL) == HIGH && digitalRead(BIRR) == HIGH) {
-        if (isArrayEmpty() && robot_x == 1) {    
-            goingLeft();
-            delay(2000);
-            forward();
-            delay(2000);
-            motorStop();    
-        } else {
-            turnLeft();
+            
+            if(robot_x == 1 && robot_y == 5){ 
+              yFlag = 1;                             
+              delay(1000);
+              goingLeft();
+              delay(1000);
+            } else if(robot_y != 5) { 
+              delay(1000);
+              goingLeft(); 
+              }
             delay(1000);
-            if (robot_x == 1) {
-                robot_y -= 1;
-                robot_x = 0;
-                Serial.print("현재 로봇좌표 : X : ");
-                Serial.println(robot_x);
-                Serial.print("현재 로봇좌표 : Y : ");
-                Serial.println(robot_y);
-            } else if (robot_x == 0) {
-                robot_y += 1;
-                robot_x = 1;
-                if (robot_y == 5) {
-                    robot_y -= 1;
-                }
-                Serial.print("현재 로봇좌표 : X : ");
-                Serial.println(robot_x);
-                Serial.print("현재 로봇좌표 : Y : ");
-                Serial.println(robot_y);
-            }
-            checkLineTraisor(); // 이동 위치 보정 후 재확인
-        }
-    }
 }
+
+void setup() {
+    Serial.begin(9600);
+    Wire.begin();
+
+    while (!huskylens.begin(Wire)) {
+        Serial.println("HuskyLens 초기화 실패. 연결을 확인하세요.");
+        Serial.println("1. HuskyLens의 'Protocol Type' 설정이 I2C인지 확인하세요.");
+        Serial.println("2. HuskyLens와 Tetrix Prizm 보드 간의 연결을 확인하세요.");
+        delay(1000); // 1초 대기
+    }
+    Serial.println("HuskyLens 초기화 성공!");
+
+    // 기본 보드 및 보조 컨트롤러 초기화
+    prizm.PrizmBegin();
+    prizm.setMotorInvert(2, 1);
+    exc.setMotorInvert(1, 1, 1);
+    prizm.setServoSpeed(3, 50);
+    prizm.setServoPosition(3, 60);
+    exc.setMotorInvert(2, 1, 1);
+
+    // 정면부 라인트레이서 초기화
+    pinMode(FR, INPUT);
+    pinMode(FL, INPUT);
+    pinMode(BR, INPUT);
+
+}
+
 
 void loop() {
-    backward();
-    delay(2000);
-    goingLeft();
-    delay(2000);
-    checkLineTraisor();
-}
+      // if(firstMoving == 0){
+      //   backward();
+      //   delay(4500);
+      //   goingLeft();
+      //   delay(4500);
+      //   firstMoving += 1;
+      //   }
+
+        int F_R = analogRead(FR);
+        int F_L = analogRead(FL);
+        int B_R = !digitalRead(BR);
+        // int B_L = !digitalRead(BL);
+     
+        Serial.print("직진 오른쪽 센서 : ");
+        Serial.println(F_R);
+        Serial.print("직진 왼쪽 센서 : ");
+        Serial.println(F_L);
+        Serial.print("옆길 오른쪽 센서 : ");
+        Serial.println(B_R);
+        // Serial.print("옆길 왼쪽 센서 : ");
+        // Serial.println(B_L);
+        // Serial.println("------------------");
+        delay(100);
+        
+        if(F_R >= 300 && F_L >= 300){
+          goingLeft();
+        }else if(F_R <= 300 && F_L >= 600){     
+          Serial.println("헐 이거 작동함");
+            exc.setMotorSpeeds(1, 20, -20);
+            exc.setMotorSpeeds(2, -20, 20);
+          }else if(F_R >= 600 && F_L <= 300){
+            Serial.println("헐 이게 왜 작동함??");
+            exc.setMotorSpeeds(1, -20, 20);
+            exc.setMotorSpeeds(2, 20, -20);
+        }
+
+        // 양쪽 라인 및 정면 라인 센서 발견시
+        if (F_R >= 250 && F_L >= 250 && B_R == 1) 
+        {
+
+            motorStop();
+            delay(1000);            
+            Serial.println("Line detected, stopping motors.");
+            if (robot_x == 0) {      
+                robot_y += 1;
+                Serial.print("현재 로봇좌표 : X : ");
+                Serial.println(robot_x);
+                Serial.print("현재 로봇좌표 : Y : ");
+                Serial.println(robot_y);
+                checkHuskyLens();                 
+            } else if (robot_x == 1) {
+                if(yFlag == 1) {                   
+                  robot_y -= 1; 
+                }
+                Serial.print("현재 로봇좌표 : X : "
