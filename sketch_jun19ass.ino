a34dbcab09befe9b94ea0daf02354adeb01562c4
seunghyun <rs_next@naver.com>
2024-06-21 20:47:06 +0900

    get object success

--- sketch_jun19ass.ino
+++ sketch_jun19ass.ino
@@ -1,4 +1,3 @@
-
 #include <Wire.h>
 #include <HUSKYLENS.h>
 #include <PRIZM.h> // 테트릭스 프리즘 보드 초기화
@@ -25,6 +24,10 @@ int middleValue = 512;
 int yFlag = 0;
 int xFlag = 0;
 int popObjectFlag = 0;
+int checkArray = 0;
+int gotObject = 0;
+int initGet = 0;
+
 struct Pillar {
     Color color;
     int pillar_x;
@@ -62,7 +65,37 @@ double checkDistance(){
   Serial.println(distance);
   return distance;
 }
+bool checkSameObject(){        
+    Color nowColor = objectArray[checkArray];
+    int target_x = -1;
+    int target_y = -1;
+ // nowColor와 동일한 색을 가진 Pillar를 찾기
+     for (int i = 0; i < 3; i++) {
+         if (pillars[i].color == nowColor) {
+            target_x = pillars[i].pillar_x;
+            target_y = pillars[i].pillar_y;
+            Serial.print("target_x : ");
+            Serial.println(target_x);
+            Serial.print("target_y : ");
+            Serial.println(target_y);
+            Serial.print("now robot_x : ");
+            Serial.println(robot_x);
+            Serial.print("now robot_y : ");
+            Serial.println(robot_y);
+         }
 
+     }
+
+     if(target_x == robot_x && target_y == robot_y){
+      return true;
+     }else{
+      return false;
+     }
+         
+}
+void popObject(){
+  // getObject -= 1;
+}
 void getObject(){
     //if(objectColorCount != 0){
     // prizm.setMotorSpeed(1, -300); // 레일 올리기
@@ -77,39 +110,49 @@ void getObject(){
   //}
 
   //체크 거리해서 몇 센티까지 오는지 ?
-  goingLeft(); // 집기 여유공간
+  exc.setMotorSpeeds(1, 90, 90); // 살짝 이동
+  exc.setMotorSpeeds(2, 90, 90); // 살짝 이동
   delay(50);
+  motorStop(); // 이동 종료
   if(objectColorCount == 0){ //첫 오브젝트 집기
     prizm.setServoPosition(1,0); // 집게 풀기
-    delay(700);
+    delay(2000);
     while(checkDistance() > 14){
-      exc.setMotorSpeeds(1, -20, 20);
-      exc.setMotorSpeeds(2, 20, -20);
-    }
+      exc.setMotorSpeeds(1, -30, 30); // 오브젝트로 가기
+      exc.setMotorSpeeds(2, 30, -30);
+    }    
+    exc.setMotorSpeeds(1, 0, 0);  //그만 가기
+    exc.setMotorSpeeds(2, 0, 0);
     delay(500);
     prizm.setServoPosition(1,180); // 집게 조이기  
-    delay(700);
+    delay(2000);
 
   }else if(objectColorCount != 0){ // 두, 세번 째 오브젝트 집기
-    prizm.setMotorSpeed(1, -300); // 레일 올리기
-    delay(2000);
+    prizm.setMotorSpeed(1, -300); // 레일 올리기 시작
+    delay(2500);
+    prizm.setMotorSpeed(1, 0); // 레일 올리기 종료
     while(checkDistance() > 14){
-      exc.setMotorSpeeds(1, -20, 20); // 오브젝트로 가기
-      exc.setMotorSpeeds(2, 20, -20);
+      exc.setMotorSpeeds(1, -30, 30); // 오브젝트로 가기
+      exc.setMotorSpeeds(2, 30, -30);
     }
+    exc.setMotorSpeeds(1, 0, 0); //오브젝트 이동 종료
+    exc.setMotorSpeeds(2, 0, 0); // 오브젝트 이동 종료
     delay(500);
-    prizm.setServoPosition(1,0); // 집게 풀기
-    delay(700);
-    prizm.setMotorSpeed(1, 300); // 레일 내리기
+    prizm.setServoPosition(1,0); // 집게 풀기 진행
     delay(2000);
+    prizm.setMotorSpeed(1, 300); // 레일 내리기 시작
+    delay(2500);
+    prizm.setMotorSpeed(1, 0); //레일 내리기 종료
     prizm.setServoPosition(1,180); // 집게 조이기      
     delay(700);
   }
-while(checkDistance() < 23){
-      exc.setMotorSpeeds(1, 20, -20);
-      exc.setMotorSpeeds(2, -20, 20);
-}
-
+  while(checkDistance() < 23){
+        exc.setMotorSpeeds(1, 30, -30);
+        exc.setMotorSpeeds(2, -30, 30);
+  }
+  exc.setMotorSpeeds(1, 0, 0);
+  exc.setMotorSpeeds(2, 0, 0);
+  gotObject += 1;
 }
 
 void forward() {
@@ -196,118 +239,32 @@ void checkHuskyLens(){
                         case RED:
                             pillars[0].pillar_x = nowX;
                             pillars[0].pillar_y = nowY;
+                            Serial.print("RED X: ");
+                            Serial.println(nowX);
+                            Serial.print("RED Y: ");
+                            Serial.println(nowY);
                             break;
                         case GREEN:
                             pillars[1].pillar_x = nowX;
                             pillars[1].pillar_y = nowY;
+                            Serial.print("GREEN X: ");
+                            Serial.println(nowX);
+                            Serial.print("GRREN Y: ");
+                            Serial.println(nowY);
                             break;
                         case BLUE:
                             pillars[2].pillar_x = nowX;
                             pillars[2].pillar_y = nowY;
+                            Serial.print("BLUE X: ");
+                            Serial.println(nowX);
+                            Serial.print("BLUE Y: ");
+                            Serial.println(nowY);
                             break;
                         default:
                             break;
                     }
 
                    if (!isColorInArray(ObjectColor)) {
-                      if (objectColorCount < ARRAY_SIZE) {
+                      if (objectColorCount < ARRAY_SIZE) {  
                           delay(500);
-                          getObject();
-                          objectArray[objectColorCount++] = ObjectColor;
-                          
-                      } else {
-                          Serial.println("Color array limit exceeded.");
-                          Serial.println("컬러 배열 한도초과");
-                      }
-                  } else {
-                      Serial.println("ObjectColor already in array.");
-                      Serial.println("ObjectColor가 배열에 이미 존재합니다.");
-                  }
-               }
-            }
-            
-            if(robot_x == 1 && robot_y == 5){ 
-              yFlag = 1;                             
-              delay(1000);
-              goingLeft();
-              delay(1000);
-            } else if(robot_y != 5) { 
-              delay(1000);
-              goingLeft(); 
-              }
-            if(xFlag == 1) { xFlag = 0; }  
-            delay(1000);
-}
-
-void setup() {
-    Serial.begin(9600);
-    Wire.begin();
-
-    while (!huskylens.begin(Wire)) {
-        Serial.println("HuskyLens 초기화 실패. 연결을 확인하세요.");
-        Serial.println("1. HuskyLens의 'Protocol Type' 설정이 I2C인지 확인하세요.");
-        Serial.println("2. HuskyLens와 Tetrix Prizm 보드 간의 연결을 확인하세요.");
-        delay(1000); // 1초 대기
-    }
-    Serial.println("HuskyLens 초기화 성공!");
-
-    // 기본 보드 및 보조 컨트롤러 초기화
-    prizm.PrizmBegin();
-    // prizm.setMotorInvert(2, 1);
-    // exc.setMotorInvert(1, 1, 1);
-    // prizm.setServoSpeed(1, 50);
-    // prizm.setMotorSpeed(1, 50);
-    // prizm.setServoPosition(3, 60);
-    // exc.setMotorInvert(2, 1, 1);
-
-    // 정면부 라인트레이서 초기화
-    pinMode(FR, INPUT);
-    pinMode(FL, INPUT);
-    pinMode(BR, INPUT);
-
-}
-
-
-void loop() {
-      // if(firstMoving == 0){
-      //   backward();
-      //   delay(4500);
-      //   goingLeft();
-      //   delay(4500);
-      //   firstMoving += 1;
-      //   }
-
-        int F_R = analogRead(FR);
-        int F_L = analogRead(FL);
-        int B_R = !digitalRead(BR);
-   int checkArray = 0;if(popObjectFlag == 1){
-  int checkArray = 0;
-  while(1){
-    if(checkArray == 3) break;    
-    Color nowColor = objectArray[checkArray];
-    
-  }
-}      
-     // nowColor와 동일한 색을 가진 Pillar를 찾기
-            for (int i = 0; i < 4; i++) {
-                if (pillars[i].color == nowColor) {
-                    std::cout << "Pillar found for color: " << nowColor << std::endl;
-                    std::cout << "Pillar x: " << pillars[i].pillar_x << std::endl;
-                    std::cout << "Pillar y: " << pillars[i].pillar_y << std::endl;
-                    break;
-                }
-            }
-
-            checkArray++; // nowColor와 동일한 색을 가진 Pillar를 찾기
-            for (int i = 0; i < 4; i++) {
-                if (pillars[i].color == nowColor) {
-                    std::cout << "Pillar found for color: " << nowColor << std::endl;
-                    std::cout << "Pillar x: " << pillars[i].pillar_x << std::endl;
-                    std::cout << "Pillar y: " << pillars[i].pillar_y << std::endl;
-                    break;
-                }
-            }
-
-            checkArray++;         
-      
-         
+                          getObjectif()popObjectFlag == 0{}{}}
